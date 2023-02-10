6196ee7e9570d0f86579f3f6fed4a340ec972e95
vazgen-gasparyan <gvazgen@outlook.com>
2023-02-10 20:46:18 +0400

    update the header and footer on the frontend

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,46 +1,20 @@
-button_btn = document.querySelector('.button-btn')
-
-button_btn.onclick = function() {
-    this.innerHTML = '<div class="loader"></div>';
-    setTimeout(() => {
-        this.innerHTML = 'Error :)';
-        this.style = 'background: #ff7200; color: #fff; pointer-events: none';
-    }, 5000);
+function doSomething(){
+  document.getElementById("test").innerHTML = "Գնումներ կարող եք կատարել զանգի կամ հաղորդագրության միջոցով:Կայքի Ներքևի հատվածում կարող եք  գտնել ձեզ անհրաժեշտ ապրանքները և դրանց գները";
 }
 
+// Type - categories
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
 
-const links = document.querySelectorAll('.scroll-btn');
+const optionsList = document.querySelectorAll(".option");
 
-for (const link of links) {
-    link.addEventListener('click', clickHandlear);
-}
-
-function clickHandlear(e) {
-    e.preventDefault();
-    const href = this.getAttribute('href');
-    const offsetTop = document.querySelector(href).
-    offsetTop;
-
-    scroll({
-        top: offsetTop,
-        behavior: 'smooth'
-    });
-}
-
-// Type
-
-const selected = document.querySelector('.selected');
-const optionsContainer = document.querySelector('.options-container');
-
-const optionsList = document.querySelectorAll('.option');
-
-selected.addEventListener('click', () => {
-  optionsContainer.classList.toggle('active');
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
 });
 
 optionsList.forEach(o => {
-  o.addEventListener('click', () => {
-    selected.innerHTML = o.querySelector('label').innerHTML;
-    optionsContainer.classList.remove('active');
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
   });
 });
